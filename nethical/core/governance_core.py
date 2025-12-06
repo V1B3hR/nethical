ce90288718121dc2a80325d2f676ca0ece5a2f7e
V1B3hR <83901968+V1B3hR@users.noreply.github.com>
2025-12-06 12:16:21 +0000

    Update governance_core.py

--- nethical/core/governance_core.py
+++ nethical/core/governance_core.py
@@ -891,14 +891,11 @@ class EnhancedSafetyGovernance:
 
     # -------- Detector Execution Offloading --------
 
-    async def _run_detector_cpu_bound(
+        async def _run_detector_cpu_bound(
         self, detector: BaseDetector, action: AgentAction
     ) -> List[SafetyViolation]:
         start = time.time()
-        if detector.cpu_bound:
-            res = await asyncio.to_thread(lambda: asyncio.run(detector.detect_violations(action)))
-        else:
-            res = await detector.detect_violations(action)
+        res = await detector.detect_violations(action)
         elapsed = time.time() - start
         timing = self.metrics["detector_timing"].setdefault(detector.name, [])
         timing.append(elapsed)
